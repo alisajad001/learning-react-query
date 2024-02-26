1b70b6d26a2f6679a11df8116f236c02a7ff8d5c
Ali Sajad Ahmadi <126584124+alisajad001@users.noreply.github.com>
2024-02-26 19:57:09 +0430

    feat: fetch cat fact data

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
 import { Routes, Route } from 'react-router-dom';
+import { QueryClientProvider, QueryClient } from 'react-query';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import CatFact from './components/CatFact';
 
+const queryClient = new QueryClient();
+
 const App = () => {
   return (
-    <div>
+    <QueryClientProvider client={queryClient}>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/cat-fact" element={<CatFact />} />
       </Routes>
-    </div>
+    </QueryClientProvider>
   );
 };
 
--- src/components/CatFact.jsx
+++ src/components/CatFact.jsx
@@ -1,5 +1,37 @@
+import axios from 'axios';
+import { useQuery } from 'react-query';
+
 const CatFact = () => {
-  return <div>CatFact</div>;
+  const fetchCatData = () => {
+    return axios.get('https://catfact.ninja/fact');
+  };
+
+  const { isLoading, error, data, refetch } = useQuery(
+    'cat-fact',
+    fetchCatData,
+    {
+      refetchOnMount: false,
+      refetchInterval: 10000,
+    },
+  );
+  return (
+    <div>
+      <h1>Cat Fact</h1>
+      <h3>About this component</h3>
+      <ul>
+        <li>01 - Fetches data from an API</li>
+        <li>02 - Uses React Query to fetch data</li>
+        <li>03 - Displays data in a list</li>
+        <li>04 - Re-fetches data every 10 seconds</li>
+        <li>05 - Handles errors</li>
+        <li>06 - Refetches on button click</li>
+      </ul>
+      <button onClick={refetch}>Get Random Cat Fact</button>
+      {isLoading && <p>Loading...</p>}
+      {error && <p>Error: {error.message}</p>}
+      {data && <h2>{data.data.fact}</h2>}
+    </div>
+  );
 };
 
 export default CatFact;
